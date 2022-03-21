bdc5d093f85d69cb00e5ec83f76dfe2867c81962
lilo <eunehghim@gmail.com>
2022-03-21 23:55:08 +0900

    [Create] redis-test app

---
+++ redis_test/views.py
@@ -0,0 +1,14 @@
+from cgitb import text
+from django.shortcuts   import render
+from django.conf    import settings
+from django.core.cache.backends.base    import DEFAULT_TIMEOUT
+from django.views.decorators.cache  import cache_page
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+@cache_page(CACHE_TTL)
+def home(request):
+    test = 'hello'
+    return render(request, 'redis_test/home.html', {'text':text})
+
+
