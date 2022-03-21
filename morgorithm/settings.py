330c3c2beb79a25e52a343b63a104d977bb7386d
lilo <eunehghim@gmail.com>
2022-03-22 01:18:43 +0900

    [ADD] redis-test

--- morgorithm/settings.py
+++ morgorithm/settings.py
@@ -107,7 +107,7 @@ CACHES = {
         'LOCATION' : 'redis://127.0.0.1:6379/1',
         'OPTIONS' : {
             'CLIENT_CLASS' : 'django_redis.client.DefaultClient',
-            'TIMEOUT' : 5,
+            # 'TIMEOUT' : 5,
         }
     }
 }
